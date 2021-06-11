df657de87ebd97f0205178504b997bce75f00f63
ozoin <dreka655@gmail.com>
2021-06-11 15:07:11 +0300

    Portfolio

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,14 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import myImg from "../../Assets/avatar.svg";
-import Tilt from "react-parallax-tilt";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
-
 function Home2() {
   return (
     <Container fluid className="home-about-section" id="about">
@@ -45,9 +37,9 @@ function Home2() {
             </p>
           </Col>
           <Col md={4} className="myAvtar">
-            <Tilt>
+           
               <img src={myImg} className="img-fluid" alt="avatar" />
-            </Tilt>
+            
           </Col>
         </Row>
       </Container>
